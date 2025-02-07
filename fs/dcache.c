71a14785ea81a14e12954e65ba8b0d9099df841a
kvsnr113 <kvsnrprojkt113@gmail.com>
2025-02-07 22:30:12 +0700

    dcache: fixup android_kabi_reserve issues for susfs
    Signed-off-by: kvsnr113 <kvsnrprojkt113@gmail.com>

--- fs/dcache.c
+++ fs/dcache.c
@@ -2173,9 +2173,9 @@ seqretry:
 		if (dentry_cmp(dentry, str, hashlen_len(hashlen)) != 0)
 			continue;
 #ifdef CONFIG_KSU_SUSFS_SUS_PATH
-		if (dentry->d_inode && unlikely(dentry->d_inode->i_state & 16777216) && likely(current_cred()->user->android_kabi_reserved2 & 16777216)) {
-			continue;
-		}
+			if (dentry->d_inode && unlikely(dentry->d_inode->i_state & INODE_STATE_SUS_PATH) && likely(current->susfs_task_state & TASK_STRUCT_NON_ROOT_USER_APP_PROC)) {
+				continue;
+			}
 #endif
 
 		*seqp = seq;
@@ -2261,7 +2261,7 @@ struct dentry *__d_lookup(const struct dentry *parent, const struct qstr *name)
 			continue;
 
 #ifdef CONFIG_KSU_SUSFS_SUS_PATH
-		if (dentry->d_inode && unlikely(dentry->d_inode->i_state & 16777216) && likely(current_cred()->user->android_kabi_reserved2 & 16777216)) {
+		if (dentry->d_inode && unlikely(dentry->d_inode->i_state & INODE_STATE_SUS_PATH) && likely(current->susfs_task_state & TASK_STRUCT_NON_ROOT_USER_APP_PROC)) {
 			continue;
 		}
 #endif
